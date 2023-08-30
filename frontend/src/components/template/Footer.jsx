63a09d1c9f22eec2677b29839368875eed57c362
heroshg <heros.goodoy@gmail.com>
2023-08-29 22:28:02 -0300

    subindo arquivos do projeto

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    <span>
+        Desenvolvido com <i className='fa fa-heart text-danger'></i> por
+        <strong>H<span className='text-danger'>3</span>ros</strong>
+    </span>
+</footer>
